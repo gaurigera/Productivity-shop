6ed1c2d1dd7695339f585d854dd07dbdb3a5eeba
Gowri <geragauri@gmail.com>
2023-07-29 21:42:00 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-TASKS = ["Drink water","Eat food","DO work","Get sleep"]
-
 @app.route("/")
 def index():
-    return render_template("home.html",task= TASKS)
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
